3bbad9e4ae62827f9530a635b7ff05b425759981
ahmad Saleh <ahmadtmsaleh@gmail.com>
2018-07-02 22:49:33 +0300

    leave request alert
    added leave reques alert

--- src/pages/newleave/newleave.ts
+++ src/pages/newleave/newleave.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { LeaveHistoryService } from '../services/leaveHistory';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-newleave',
   templateUrl: 'newleave.html',
 })
 export class NewleavePage {
-  constructor(private nlService:LeaveHistoryService){}
+  constructor(private nlService:LeaveHistoryService
+    ,public alertCtrl: AlertController){}
 
   onAddLeave(form: NgForm){
     this.nlService.newLeave(form.value.type,form.value.startDate,form.value.endDate);
-    form.reset();
+    this.showAlert();
+    form.reset();    
   }
+
+showAlert(){
+  const alert =this.alertCtrl.create({
+    title:'Leave Alert',
+    subTitle:'Leave Request Has been submitted for approval!',
+    buttons:['OK']
+  });
+  alert.present();
+}
 }
